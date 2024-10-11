8429bec3bc447869dfb580d3a001e0810b1c3833
mega025 <xinjiepan123@gmail.com>
2024-10-11 14:30:11 +0200

    login

--- app/src/main/java/com/mega025/first/SplashScreen.java
+++ app/src/main/java/com/mega025/first/SplashScreen.java
@@ -1,12 +1,25 @@
 package com.mega025.first;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+
+import java.lang.annotation.Annotation;
+
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -20,5 +33,33 @@ public class SplashScreen extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        launchMain();
+
+        Animation gradientColor = AnimationUtils.loadAnimation(this, R.anim.gradient_color);
+        ImageView splashlogo = findViewById(R.id.logoSplash);
+        TextView splashAppName = findViewById(R.id.splashAppName);
+        splashlogo.startAnimation(gradientColor);
+        splashAppName.startAnimation(gradientColor);
+        ImageView glideBackground = findViewById(R.id.splashGlideBackground);
+
+
+        Glide.with(this)
+                .load("https://images.unsplash.com/photo-1515310787031-25ac2d68610d?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+                .transition(DrawableTransitionOptions.withCrossFade(1000))
+                .centerCrop()
+                .into(glideBackground);
+
+
+    }
+    public void launchMain(){
+        new Handler(Looper.myLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        },3000);
     }
 }
