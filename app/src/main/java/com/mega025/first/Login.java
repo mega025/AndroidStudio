c31822eb45860ddbee761139c20eeb810b20d468
mega025 <xinjiepan123@gmail.com>
2024-10-15 11:21:23 +0200

    Registro e Iniciar sesión en la app

--- app/src/main/java/com/mega025/first/Login.java
+++ app/src/main/java/com/mega025/first/Login.java
@@ -1,6 +1,10 @@
 package com.mega025.first;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +19,33 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+
+        Button logiButton = findViewById(R.id.loginBotonEntrar);
+        TextView loginRegistro = findViewById(R.id.loginBotonRegistrar);
+
+        logiButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchActivity();
+            }
+        });
+    loginRegistro.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            launchRegistro();
+        }
+    });
+
+    }
+    public void launchActivity(){
+        Intent intent = new Intent(Login.this,MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+    public void launchRegistro(){
+        Intent intent = new Intent(Login.this,register.class);
+        startActivity(intent);
 
     }
 }
