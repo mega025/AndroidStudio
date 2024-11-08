bac098a50dfe2789c2fb5f4113e9dab69cf1d38b
mega025 <xinjiepan123@gmail.com>
2024-11-08 20:17:20 +0100

    No se cuales son los errores que me esta dando desde casa

--- app/src/main/java/com/mega025/calculadora/SplashScreen.java
+++ app/src/main/java/com/mega025/calculadora/SplashScreen.java
@@ -11,6 +11,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.mega025.first.R;
+
 public class SplashScreen extends AppCompatActivity {
 
     @Override
