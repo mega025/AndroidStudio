97fc7165fde06f45514f4847d6961ecc5780cbfa
mega025 <xinjiepan123@gmail.com>
2024-11-15 14:03:21 +0100

    Empezamos con fragment

---
+++ app/src/main/java/com/mega025/misaficiones/Aficiones.java
@@ -0,0 +1,55 @@
+package com.mega025.misaficiones;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.mega025.misaficiones.databinding.ActivityAficionesBinding;
+import com.mega025.misaficiones.ui.fragments.Paginador;
+
+public class Aficiones extends AppCompatActivity {
+
+    private ActivityAficionesBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityAficionesBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Paginador paginador = new Paginador(this, getSupportFragmentManager());
+        ViewPager viewPager = binding.viewPager;
+        viewPager.setAdapter(paginador);
+        }
+
+        public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_sobre_mi, menu);
+        return true;
+        }
+
+        public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        int id = item.getItemId();
+        if (id == R.id.favButton){
+            Toast toast = Toast.makeText(this, "como me gusta mis aficciones", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+        if (id == R.id.aboutMeButton){
+           // Intent intent = new Intent(Aficiones.this, Sobremi.class);
+                    //startActivity(intent);
+        }
+        if (id == R.id.myCodeButton){
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(""));
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+        }
+}
