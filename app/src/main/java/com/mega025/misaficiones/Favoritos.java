cacbb28f56291e90dba5a72bce6fea98c771d171
Xinjie <xinjiepan123@gmai.com>
2024-11-21 19:12:30 +0100

    Fustrado por que no se hacer el fav

--- app/src/main/java/com/mega025/misaficiones/Favoritos.java
+++ app/src/main/java/com/mega025/misaficiones/Favoritos.java
@@ -2,19 +2,9 @@ package com.mega025.misaficiones;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.tabs.TabLayout;
-
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-
-import com.mega025.misaficiones.ui.fragments.PagginadorFav;
-import com.mega025.misaficiones.ui.main.SectionsPagerAdapter;
 import com.mega025.misaficiones.databinding.ActivityFavoritosBinding;
 
 public class Favoritos extends AppCompatActivity {
@@ -25,11 +15,5 @@ public class Favoritos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityFavoritosBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        PagginadorFav paginador = new PagginadorFav(this, getSupportFragmentManager());
-        ViewPager viewPager = binding.viewPager;
-        viewPager.setAdapter(paginador);
     }
 }
