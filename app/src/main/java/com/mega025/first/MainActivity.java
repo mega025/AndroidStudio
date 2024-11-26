703f7de3eb1d1019c13ffd39f970fe0114f91e40
mega025 <xinjiepan123@gmail.com>
2024-11-26 09:33:04 +0100

    dialog

--- app/src/main/java/com/mega025/first/MainActivity.java
+++ app/src/main/java/com/mega025/first/MainActivity.java
@@ -1,17 +1,17 @@
 package com.mega025.first;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
-import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,6 +29,30 @@ public class MainActivity extends AppCompatActivity {
 
         Bienvenido.setText("Bienvenido a la mejor APP de senderismo " + name);
 
+
+        Button callDialogButton  = findViewById(R.id.verMas);
+        callDialogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getApplicationContext());
+                dialogBuilder.setMessage("hola" + name);
+                dialogBuilder.setCancelable(true);
+                dialogBuilder.setPositiveButton("Si soy yo", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+                dialogBuilder.setNegativeButton("no", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+                AlertDialog alertDialog = dialogBuilder.create();
+                alertDialog.show();
+            }
+        });
 
     }
 }
